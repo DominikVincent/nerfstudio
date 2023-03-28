a9973f1098342c3272604c510472ee376bcbd3b0
Dominik Hollidt <dhollidt@mint.csail.mit.edu>
2023-03-27 21:55:15 -0400

    fix weird bugs in eval sweep

--- train_scripts/eval_sweep.py
+++ train_scripts/eval_sweep.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python
 
-import wandb
-from pathlib import Path
-import yaml
-from typing import Union
 import argparse
 import multiprocessing
-import time
-from scripts.eval import ComputePSNR
-from nerfstudio.utils import writer
 import subprocess
+import time
+from pathlib import Path
+from typing import Union
+
+import yaml
+
+import wandb
+
+# from nerfstudio.utils import writer
+
+# from scripts.eval import ComputePSNR
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--proj_name", help="The wandb proj name", type=str, default="dhollidt/mae-models-project")
 parser.add_argument("--sweep_id", help="The wandb sweep id", type=str, default="kfsdevg7")
-parser.add_argument("--eval_config", help="The nesf eval config", type=str, default="/data/vision/polina/projects/wmh/dhollidt/documents/nerf/data/nesf_test_config_test.json")
+parser.add_argument(
+    "--eval_config",
+    help="The nesf eval config",
+    type=str,
+    default="/data/vision/polina/projects/wmh/dhollidt/documents/nerf/data/nesf_test_config_test.json",
+)
 
 FIX_CONFIG = True
+
 
 def get_sweep_runs(sweep_id, project_name):
     api = wandb.Api()
@@ -25,11 +35,16 @@ def get_sweep_runs(sweep_id, project_name):
 
 
 def sweep_run_to_path(run):
-    return Path(run.config["output_dir"]) / run.config["experiment_name"].strip("/") / run.config["method_name"] / run.config["timestamp"]
+    return (
+        Path(run.config["output_dir"])
+        / run.config["experiment_name"].strip("/")
+        / run.config["method_name"]
+        / run.config["timestamp"]
+    )
 
 
-def ns_eval(config_path, output_path):
-    command = f"ns-eval --load-config {config_path} --output-path {output_path} --use-wandb"
+def ns_eval(config_path, output_path, name=""):
+    command = f"ns-eval --load-config {config_path} --output-path {output_path} --use-wandb --name {name}"
 
     # Execute the command and capture the output
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -38,7 +53,7 @@ def ns_eval(config_path, output_path):
     while True:
         output = process.stdout.readline().decode()
         error = process.stderr.readline().decode()
-        if output == '' and error == '' and process.poll() is not None:
+        if output == "" and error == "" and process.poll() is not None:
             break
         if output:
             print(output.strip())
@@ -47,20 +62,26 @@ def ns_eval(config_path, output_path):
 
     # Wait for the command to finish and get the return code
     return_code = process.wait()
+    return return_code
 
-def eval_run(path: Path, eval_set: Union[None, Path]=None, wandb_config = None, gpu_index: int = 0):
-    print("GPU: ", gpu_index, "Path: ", path, "writers: ", writer.EVENT_WRITERS)
+
+# def eval_run(path: Path, eval_set: Union[None, Path] = None, run, gpu_index: int = 0):
+#   wandb_config = wandb_run.config
+#   wandb_name = wandb_run.name + "_eval"
+def eval_run(path: Path, eval_set: Union[None, Path] = None, wandb_config=None, wandb_name=None, gpu_index: int = 0):
+    # print("GPU: ", gpu_index, "Path: ", path, "writers: ", writer.EVENT_WRITERS)
     import os
+
     os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_index)
-    
-    out_path = path / "auto_eval_config.yml"    
+
+    out_path = path / "auto_eval_config.yml"
     input_path = path / "config.yml"
-    
+
     config = yaml.load(input_path.read_text(), Loader=yaml.Loader)
-    
+
     if eval_set is not None:
         config.pipeline.datamanager.dataparser.data_config = eval_set
-    
+
     if FIX_CONFIG:
         for key, value in wandb_config["pipeline"]["model"].items():
             if key.startswith("_"):
@@ -75,7 +96,7 @@ def eval_run(path: Path, eval_set: Union[None, Path]=None, wandb_config = None, 
                 print(f"WARNING: {key} not found in model config")
     # save config as yaml
     out_path.write_text(yaml.dump(config), "utf8")
-    
+
     # eval = ComputePSNR(load_config=out_path,
     #                    output_path=path / "auto_eval.json",
     #                    save_images=False,
@@ -84,26 +105,24 @@ def eval_run(path: Path, eval_set: Union[None, Path]=None, wandb_config = None, 
     # print(gpu_index, ": Reseting writers: ", writer.EVENT_WRITERS)
     # writer.reset_writer()
     # print(gpu_index, ": Reset writers", writer.EVENT_WRITERS)
-    ns_eval(out_path, path / "auto_eval.json")
-    
+    ns_eval(out_path, path / "auto_eval.json", name=wandb_name)
     print(" ######################### Done with: ", path, " ######################### ")
     return path
-    
-    return
-def main(project_name: str, sweep_id: str, eval_config: Union[None, Path]=None):
+
+
+def main(project_name: str, sweep_id: str, eval_config: Union[None, Path] = None):
     runs = get_sweep_runs(sweep_id, project_name)
-    pool = multiprocessing.Pool(processes=3, maxtasksperchild=1)
-    
+    pool = multiprocessing.Pool(processes=2, maxtasksperchild=1)
+
     results = []
     for i, run in enumerate(runs):
-        gpu_index = i % 3 + 1 # assign the next GPU index
+        gpu_index = i % 3 + 1  # assign the next GPU index
         path = sweep_run_to_path(run)
         print(path)
-        result = pool.apply_async(eval_run, args=(path, eval_config, run.config, gpu_index))
+        result = pool.apply_async(eval_run, args=(path, eval_config, run.config, run.name + "_test", gpu_index))
         results.append(result)
-        # eval_run(path, eval_config, run.config, gpu_index)
-    
-    
+        # eval_run(path, eval_config, run, gpu_index)
+
     pool.close()
     print("###### CLOSE POOL ######")
     pool.join()
@@ -112,7 +131,8 @@ def main(project_name: str, sweep_id: str, eval_config: Union[None, Path]=None):
     # get the results from the async calls
     final_results = [result.get() for result in results]
     print(final_results)
-    
+
+
 if __name__ == "__main__":
     args = parser.parse_args()
     proj_name = args.proj_name
