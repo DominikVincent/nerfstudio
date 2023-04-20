fix random batching bug and add radius scaling

nerfstudio/model_components/nesf_components.py
@@ -7,6 +7,7 @@ import torch
 from pointnet.models.pointnet2_sem_seg import get_model
 from rich.console import Console
 from torch import nn
+from torch.distributions import uniform
 from torch.nn import Parameter
 from torchtyping import TensorType
 
@@ -170,19 +171,19 @@ class TransformerEncoderModel(torch.nn.Module):
 class FeatureGeneratorTorchConfig(InstantiateConfig):
     _target: type = field(default_factory=lambda: FeatureGeneratorTorch)
 
-    use_rgb: bool = True
+    use_rgb: bool = False
     """Should the rgb be used as a feature?"""
     out_rgb_dim: int = 16
     """The output dimension of the rgb feature"""
 
-    use_density: bool = False
+    use_density: bool = True
     """Should the density be used as a feature?"""
-    out_density_dim: int = 8
+    out_density_dim: int = 2
 
     use_pos_encoding: bool = True
     """Should the position encoding be used as a feature?"""
 
-    use_dir_encoding: bool = True
+    use_dir_encoding: bool = False
     """Should the direction encoding be used as a feature?"""
 
     rot_augmentation: bool = True
@@ -233,6 +234,8 @@ class FeatureGeneratorTorch(nn.Module):
             self.pos_encoder = RFFEncoding(in_dim=3, num_frequencies=8, scale=10)
         if self.config.use_dir_encoding:
             self.dir_encoder = SHEncoding()
+            
+        self.learned_mask_value = torch.nn.Parameter(torch.randn(self.get_out_dim()))
 
     def forward(self, field_outputs: dict, transform_batch: dict):
         """
@@ -287,45 +290,64 @@ class FeatureGeneratorTorch(nn.Module):
             encodings.append(density)
 
         if self.config.rot_augmentation:
-            theta = torch.rand(1) * 2 * torch.pi
-
-            # Construct the rotation matrix
-            cos_theta = torch.cos(theta)
-            sin_theta = torch.sin(theta)
-            rot_matrix = torch.tensor(
-                [[cos_theta, -sin_theta, 0], [sin_theta, cos_theta, 0], [0, 0, 1]],
-                device=device,
-            )
+            batch_size = transform_batch["points_xyz"].shape[0]
+            angles = uniform.Uniform(0, 2 * torch.pi).sample((batch_size,))
+            
+            # Construct the rotation matrices from the random angles.
+            zeros = torch.zeros_like(angles)
+            ones = torch.ones_like(angles)
+            c = torch.cos(angles)
+            s = torch.sin(angles)
+            rot_matrix = torch.stack([
+                torch.stack([c, -s, zeros], dim=-1),
+                torch.stack([s, c, zeros], dim=-1),
+                torch.stack([zeros, zeros, ones], dim=-1)
+            ], dim=-2).to(transform_batch["points_xyz"].device)
+    
         else:
             rot_matrix = torch.eye(3, device=device)
 
         if self.config.use_pos_encoding:
             positions = transform_batch["points_xyz"]
-
-            positions_normalized = SceneBox.get_normalized_positions(positions, self.aabb)
-
-            if self.config.rot_augmentation:
-                positions_normalized = torch.matmul(positions_normalized, rot_matrix)
-
-            # normalize positions at 0 mean and within unit ball
-            mean = torch.mean(positions_normalized, dim=1).unsqueeze(1)
-            dist = torch.norm(positions_normalized - mean, dim=-1).max()
             
-            positions_normalized = (positions_normalized - mean) / dist
+            
+            
+            if self.config.rot_augmentation:
+                # TODO consider turning that off if not self.training()
+                positions = torch.matmul(positions, rot_matrix)
+                # normalize postions to be within scene bounds  self.aabb: Tensor[2,3]
+                # TODO if needed add the normalizeation for now hardcode clamp
+                positions = torch.clamp(positions, self.aabb[0], self.aabb[1])
+                
+            positions = cast(TensorType, positions)
+            # The positions need to be in [0,1] for the positional encoding
+            positions_normalized = SceneBox.get_normalized_positions(positions, self.aabb)
+            transform_batch["points_xyz"] = positions_normalized
+            
+            # assert that the points are between 0 and 1
+            assert(torch.all(positions_normalized >= 0.0))
+            assert(torch.all(positions_normalized <= 1.0))
+            
+            # normalize positions at 0 mean and within unit ball
+            # mean = torch.mean(positions_normalized, dim=1).unsqueeze(1)
+            # dist = torch.norm(positions_normalized - mean, dim=-1).max()
+            
+            # positions_normalized = (positions_normalized - mean) / dist
             if self.config.visualize_point_batch:
-                visualize_point_batch(positions_normalized)
+                visualize_point_batch(transform_batch["points_xyz"])
 
             if self.config.log_point_batch and random.random() < (1 / 5000):
-                log_points_to_wandb(positions_normalized)
+                log_points_to_wandb(transform_batch["points_xyz"])
             
             pos_encoding = self.pos_encoder(positions_normalized)
             assert not torch.isnan(pos_encoding).any()
-            encodings.append(pos_encoding)
+            # encodings.append(pos_encoding)
 
         if self.config.use_dir_encoding:
             directions = transform_batch["directions"]
             directions = get_normalized_directions(directions)
             if self.config.rot_augmentation:
+                # TODO consider turning that off if not self.training()
                 directions = torch.matmul(directions, rot_matrix)
             dir_encoding = self.dir_encoder(directions)
 
@@ -335,13 +357,16 @@ class FeatureGeneratorTorch(nn.Module):
         out = torch.cat(encodings, dim=-1)
         # out: 1, num_dense, out_dim
         # weights: num_rays, num_samples, 1
-        return out
+        
+        if "src_key_padding_mask" in transform_batch and transform_batch["src_key_padding_mask"] is not None:
+            out[transform_batch["src_key_padding_mask"]] = self.learned_mask_value
+        return out, transform_batch
 
     def get_out_dim(self) -> int:
         total_dim = 0
         total_dim += self.config.out_rgb_dim if self.config.use_rgb else 0
         total_dim += self.config.out_density_dim if self.config.use_density else 0
-        total_dim += self.pos_encoder.get_out_dim() if self.config.use_pos_encoding else 0
+        # total_dim += self.pos_encoder.get_out_dim() if self.config.use_pos_encoding else 0
         total_dim += self.dir_encoder.get_out_dim() if self.config.use_dir_encoding else 0
         return total_dim
 
@@ -352,7 +377,9 @@ class PointNetWrapperConfig(InstantiateConfig):
 
     out_feature_channels: int = 128
     """The number of features the model should output"""
-
+    
+    radius_scale: float = 0.25
+    """Pointnet has radiuses tuned for the unit sphere. This scales the radiuses to the scene size as our radius are at most sqrt(2) and in practice even smaller"""
 
 class PointNetWrapper(nn.Module):
     def __init__(
@@ -374,7 +401,7 @@ class PointNetWrapper(nn.Module):
         self.mask_ratio = mask_ratio
 
         # PointNet takes xyz + features as input
-        self.feature_transformer = get_model(num_classes=config.out_feature_channels, in_channels=input_size + 3)
+        self.feature_transformer = get_model(num_classes=config.out_feature_channels, in_channels=input_size + 3, radius_factor=self.config.radius_scale)
         self.output_size = config.out_feature_channels
 
     def forward(self, x: torch.Tensor, batch: dict):
@@ -411,7 +438,7 @@ class SceneSamplerConfig(InstantiateConfig):
     """What is the minimum z value a point has to have"""
     xy_distance_threshold: float = 1
     """The maximal distance a point can have to z axis to be considered"""
-    max_points: int = 16384
+    max_points: int = 2<<17
     """The maximum number of points to use in one scene. If more are available after filtering, they will be randomly sampled"""
 
 
@@ -432,10 +459,10 @@ class SceneSampler:
             model (_type_): A nerf model. Currently NerfactoModel is supported..
 
         Returns:
-        - ray_samples (_type_): The ray samples for the scene which should be used.
-        - weights (_type_): The weights for the ray samples which are used.
-        - field_outputs (_type_): The field outputs for the ray samples which are used.
-        - final_mask (_type_): The mask for the ray samples which are used. Is the shape of the original ray_samples.
+        - ray_samples (_type_): The ray samples for the scene which should be used. (N_dense)
+        - weights (_type_): The weights for the ray samples which are used. (N_dense x 1)
+        - field_outputs (_type_): The field outputs for the ray samples which are used. (N_dense x dim)
+        - final_mask (_type_): The mask for the ray samples which are used. Is the shape of the original ray_samples. (N_rays x N_samples)
 
         Raises:
             NotImplementedError: _description_
@@ -470,7 +497,19 @@ class SceneSampler:
 
         return ray_samples, weights, field_outputs, final_mask
 
-    def surface_sampling(self, ray_samples, weights, field_outputs):
+    def surface_sampling(self, ray_samples: RaySamples, weights: TensorType["N_rays", "N_samples", 1], field_outputs: dict) :
+        """Samples only surface samples along the ray
+
+        Args:
+            ray_samples (_type_): N_rays x N_samples
+            weights (_type_): N_rays x N_samples x 1
+            field_outputs (_type_): N_rays x N_samples x dim
+
+        Returns:
+            ray_samples (_type_): N_rays x 1
+            weights (_type_): N_rays x 1 x 1
+            field_outputs (_type_): N_rays x 1 x dim
+        """
         cumulative_weights = torch.cumsum(weights[..., 0], dim=-1)  # [..., num_samples]
         split = torch.ones((*weights.shape[:-2], 1), device=weights.device) * 0.5  # [..., 1]
         median_index = torch.searchsorted(cumulative_weights, split, side="left")  # [..., 1]
@@ -487,13 +526,14 @@ class SceneSampler:
 
         return ray_samples, weights, field_outputs
 
-    def get_density_mask(self, field_outputs):
+    def get_density_mask(self, field_outputs) -> TensorType["N_rays", "N_samples"]:
         # true for points to keep
         density = field_outputs[FieldHeadNames.DENSITY]  # 64, 48, 1 (rays, samples, 1)
         density_mask = density > self.config.density_threshold  # 64, 48
-        return density_mask.squeeze(-1)
+        density_mask = density_mask.squeeze(-1)
+        return density_mask
 
-    def get_pos_mask(self, ray_samples):
+    def get_pos_mask(self, ray_samples) -> TensorType["N_rays", "N_samples"]:
         # true for points to keep
         points = ray_samples.frustums.get_positions()
         points_dense_mask = (points[..., 2] > self.config.z_value_threshold) & (
@@ -519,7 +559,14 @@ class SceneSampler:
         else:
             return mask
 
-    def apply_mask(self, ray_samples, weights, field_outputs, mask):
+    def apply_mask(self, ray_samples: RaySamples, weights: TensorType["N_rays", "N_samples", 1], field_outputs, mask: TensorType["N_rays", "N_samples"]) \
+    -> Tuple[RaySamples, TensorType["N_dense", 1], dict]:
+        """Applies the mask to the ray samples and field outputs
+
+        Returns:
+            ray_samples: Will be N_dense
+            field_outputs: Will be N_dense x dim
+        """
         for k, v in field_outputs.items():
             field_outputs[k] = v[mask]
 

nerfstudio/models/nesf.py
@@ -7,6 +7,7 @@ from typing import Any, Dict, List, Optional, Tuple, Type, Union
 
 import lovely_tensors as lt
 import numpy as np
+import plotly.express as px
 import plotly.graph_objs as go
 import torch
 import torch.nn.functional as F
@@ -32,10 +33,6 @@ from nerfstudio.utils.writer import put_config
 lt.monkey_patch()
 
 CONSOLE = Console(width=120)
-DEBUG_PLOT_SAMPLES = False
-DEBUG_PLOT_WANDB_SAMPLES = True
-DEBUG_FILTER_POINTS = True
-
 
 @dataclass
 class NeuralSemanticFieldConfig(ModelConfig):
@@ -57,7 +54,7 @@ class NeuralSemanticFieldConfig(ModelConfig):
 
     # feature_transformer_config: AnnotatedTransformerUnion = PointNetWrapperConfig()
     # dirty workaround because Union of configs didnt work
-    feature_transformer_model: Literal["pointnet", "custom"] = "pointnet"
+    feature_transformer_model: Literal["pointnet", "custom"] = "custom"
     feature_transformer_pointnet_config: PointNetWrapperConfig = PointNetWrapperConfig()
     feature_transformer_custom_config: TranformerEncoderModelConfig = TranformerEncoderModelConfig()
     
@@ -82,16 +79,20 @@ class NeuralSemanticFieldConfig(ModelConfig):
     density_cutoff: float = 1e8
     """Large density values might be an issue for training. Hence they can get cut off with this."""
 
-    batching_mode: Literal["sequential", "sliced", "off"] = "off"
+    batching_mode: Literal["sequential", "random", "sliced", "off"] = "random"
     """Usually all samples are fed into the transformer at the same time. This could be too much for the model to understand and also too much for VRAM.
     Hence we batch the samples:
      - sequential: we batch the samples by wrapping them sequentially into batches.
+     - random: take random permuatations of points for batching.
      - sliced: Sort points by x coordinate and then slice them into batches.
      - off: no batching is done."""
-    batch_size: int = 1024
+    batch_size: int = 1536
 
     samples_per_ray: int = 10
     """When sampling the underlying nerfs. How many samples should be taken per ray."""
+    
+    debug_show_image: bool = False
+    """Show the generated image."""
 
 
 def get_wandb_histogram(tensor):
@@ -191,6 +192,7 @@ class NeuralSemanticFieldModel(Model):
                 torch.nn.ReLU(),
             )
         self.learned_low_density_value = torch.nn.Parameter(torch.randn(output_size))
+        self.learned_pad_token = torch.nn.Parameter(torch.randn(self.feature_model.get_out_dim()))
 
         # Renderer
         if self.config.mode == "rgb":
@@ -234,7 +236,7 @@ class NeuralSemanticFieldModel(Model):
         param_groups = {
             "feature_network": list(self.feature_model.parameters()),
             "feature_transformer": list(self.feature_transformer.parameters()),
-            "learned_low_density_params": [self.learned_low_density_value],
+            "learned_low_density_params": [self.learned_low_density_value, self.learned_pad_token],
             "decoder": list(self.decoder.parameters()),
             "head": list(self.head.parameters()),
         }
@@ -255,23 +257,13 @@ class NeuralSemanticFieldModel(Model):
         model: Model = self.get_model(batch)
 
         # all but density mask are by filtered dimension
-        ray_samples, weights, field_outputs, density_mask = self.scene_sampler.sample_scene(ray_bundle, model)
+        ray_samples, weights, field_outputs_raw, density_mask = self.scene_sampler.sample_scene(ray_bundle, model)
 
         # potentially batch up and infuse field outputs with random points
-        field_outputs, transform_batch = self.batching(ray_samples, field_outputs)
-
-        if DEBUG_PLOT_SAMPLES:
-            visualize_points(transform_batch["points_xyz"])
-
-            if DEBUG_PLOT_WANDB_SAMPLES and random.random() < (1 / 5000):
-                log_points_to_wandb(
-                    transform_batch["points_xyz"],
-                    None,
-                    transform_batch["points_xyz"].shape[1],
-                )
-
+        field_outputs_raw, transform_batch = self.batching(ray_samples, field_outputs_raw)
+        
         # TODO return the transformed points
-        outs = self.feature_model(field_outputs, transform_batch)  # 1, low_dense, 49
+        outs, transform_batch = self.feature_model(field_outputs_raw, transform_batch)  # 1, low_dense, 49
 
         # assert outs is not nan or not inf
         assert not torch.isnan(outs).any()
@@ -308,15 +300,18 @@ class NeuralSemanticFieldModel(Model):
             field_encodings = self.feature_transformer(outs, batch=transform_batch)
             field_encodings = self.decoder(field_encodings)
             field_outputs = self.head(field_encodings)
+            
 
         # unbatch the data
         if self.config.batching_mode != "off":
             field_outputs = field_outputs.reshape(1, -1, field_outputs.shape[-1])
+            
             # reshuffle results
             field_outputs = field_outputs[:, transform_batch["ids_restore"], :]
-
+            
             # removed padding token
             field_outputs = field_outputs[:, : ray_samples.shape[0], :]
+            
 
         if self.config.mode == "rgb":
             # debug rgb
@@ -349,20 +344,21 @@ class NeuralSemanticFieldModel(Model):
 
             # semantics[~density_mask] = self.learned_low_density_value
             semantics[~density_mask] = torch.tensor([1.0, 0.0, 0.0, 0.0, 0.0, 0.0], device=self.device)
-            weights_all[~density_mask] = 0.0
+            weights_all[~density_mask] = 0.01
 
             semantics = self.renderer_semantics(semantics, weights=weights_all)
             outputs["semantics"] = semantics  # N, num_classes
 
             # semantics colormaps
             semantic_labels = torch.argmax(torch.nn.functional.softmax(semantics, dim=-1), dim=-1)
+            
             self.semantics.colors = self.semantics.colors.to(self.device)
             semantics_colormap = self.semantics.colors[semantic_labels].to(self.device)
             outputs["semantics_colormap"] = semantics_colormap
             outputs["rgb"] = semantics_colormap
 
             # print the count of the different labels
-            # CONSOLE.print("Label counts:", torch.bincount(semantic_labels.flatten()))
+            CONSOLE.print("Label counts:", torch.bincount(semantic_labels.flatten()))
         elif self.config.mode == "density":
             # TODO rewrite this THIS IS BROKEN AND WONT WORK
             rgb = misc["rgb"]
@@ -429,7 +425,8 @@ class NeuralSemanticFieldModel(Model):
             semantics = batch["semantics"][..., 0].long().to(self.device)
             with torch.no_grad():
                 # mIoU
-                metrics_dict["miou_" + str(batch["model_idx"])] = self.miou(outputs["semantics"], semantics)
+                metrics_dict["miou"] = self.miou(outputs["semantics"], semantics)
+                metrics_dict["miou_" + str(batch["model_idx"])] = metrics_dict["miou"]
         elif self.config.mode == "density":
             image = batch["image"].to(self.device)
 
@@ -566,6 +563,8 @@ class NeuralSemanticFieldModel(Model):
             rgb = outputs["rgb"]
             combined_rgb = torch.cat([image, rgb], dim=1)
             images_dict["img"] = combined_rgb
+            
+
 
             # Switch images from [H, W, C] to [1, C, H, W] for metrics computations
             image = torch.moveaxis(image, -1, 0)[None, ...]
@@ -603,6 +602,11 @@ class NeuralSemanticFieldModel(Model):
             miou = self.miou(outs, gt)
             metrics_dict = {"miou": float(miou.item())}
 
+        # plotly show image
+        if self.config.debug_show_image:
+            fig = px.imshow(images_dict["img"].cpu().numpy())
+            fig.show()
+                
         return metrics_dict, images_dict
 
     def set_model(self, model: Model):
@@ -637,15 +641,18 @@ class NeuralSemanticFieldModel(Model):
 
     def batching(self, ray_samples: RaySamples, field_outputs: dict):
         if self.config.batching_mode != "off":
-            W = ray_samples.shape[0]
             if self.config.batching_mode == "sequential":
                 # given the features and the density mask batch them up sequentially such that each batch is same size.
                 field_outputs, masking, ids_shuffle, ids_restore, points_pad, directions_pad = sequential_batching(
                     ray_samples, field_outputs, self.config.batch_size
                 )
+            elif self.config.batching_mode == "random":
+                field_outputs, masking, ids_shuffle, ids_restore, points_pad, directions_pad = random_batching(
+                    ray_samples, field_outputs, self.config.batch_size
+                )
             elif self.config.batching_mode == "sliced":
                 field_outputs, masking, ids_shuffle, ids_restore, points_pad, directions_pad = spatial_sliced_batching(
-                    ray_samples, field_outputs, self.config.batch_size
+                    ray_samples, field_outputs, self.config.batch_size, self.scene_box.aabb
                 )
             else:
                 raise ValueError(f"Unknown batching mode {self.config.batching_mode}")

nerfstudio/utils/nesf_utils.py
@@ -4,37 +4,12 @@ from typing import Union
 import plotly.graph_objs as go
 import torch
 from rich.console import Console
+from torchtyping import TensorType
 
 import wandb
 from nerfstudio.cameras.rays import RaySamples
 
 CONSOLE = Console(width=120)
-
-
-def filter_points(outs, density_mask, misc: dict):
-    Z_FILTER_VALUE = -2.49
-    Z_FILTER_VALUE = -1.0
-    XY_DISTANCE = 1.0
-    points_dense = misc["ray_samples"].frustums.get_positions()[density_mask]
-    points_dense_mask = (points_dense[:, 2] > Z_FILTER_VALUE) & (torch.norm(points_dense[:, :2], dim=1) <= XY_DISTANCE)
-    points_dense_mask = points_dense_mask.to(density_mask.device)
-    outs = outs[:, points_dense_mask, :]
-
-    points = misc["ray_samples"].frustums.get_positions()
-    points_mask = (points[:, :, 2] > Z_FILTER_VALUE) & (torch.norm(points[:, :, :2], dim=2) <= XY_DISTANCE).to(
-        density_mask.device
-    )
-    density_mask = torch.logical_and(density_mask, points_mask)
-    MAX_COUNT_POINTS = 16384
-    if density_mask.sum() > MAX_COUNT_POINTS:
-        CONSOLE.print("There are too many points, we are limiting to ", MAX_COUNT_POINTS, " points.")
-        # randomly mask points such that we have MAX_COUNT_POINTS points
-        outs = outs[:, :MAX_COUNT_POINTS, :]
-        true_indices = torch.nonzero(density_mask.flatten()).squeeze()
-        true_indices = true_indices[:MAX_COUNT_POINTS]
-        density_mask = torch.zeros_like(density_mask)
-        density_mask.view(-1, 1)[true_indices] = 1
-    return outs, density_mask
 
 
 def sequential_batching(ray_samples: RaySamples, field_outputs: dict, batch_size: int):
@@ -49,7 +24,7 @@ def sequential_batching(ray_samples: RaySamples, field_outputs: dict, batch_size
     masking_bottom = torch.full((padding,), True, dtype=torch.bool)
     masking = torch.cat((masking_top, masking_bottom)).to(device)
 
-    ids_shuffle = torch.randperm(W + padding)
+    ids_shuffle = torch.arange(W + padding)
     ids_restore = ids_shuffle
 
     points = ray_samples.frustums.get_positions()
@@ -60,8 +35,31 @@ def sequential_batching(ray_samples: RaySamples, field_outputs: dict, batch_size
 
     return field_outputs, masking, ids_shuffle, ids_restore, points_pad, directions_pad
 
+def random_batching(ray_samples: RaySamples, field_outputs: dict, batch_size: int):
+    device = ray_samples.frustums.origins.device
+    W = ray_samples.shape[0]
 
-def spatial_sliced_batching(ray_samples: RaySamples, field_outputs: dict, batch_size: int):
+    padding = batch_size - (W % batch_size) if W % batch_size != 0 else 0
+    for key, value in field_outputs.items():
+        field_outputs[key] = torch.nn.functional.pad(value, (0, 0, 0, padding))
+
+    masking_top = torch.full((W,), False, dtype=torch.bool)
+    masking_bottom = torch.full((padding,), True, dtype=torch.bool)
+    masking = torch.cat((masking_top, masking_bottom)).to(device)
+
+    ids_shuffle = torch.randperm(W + padding)
+    ids_restore = torch.argsort(ids_shuffle)
+
+    points = ray_samples.frustums.get_positions()
+    directions = ray_samples.frustums.directions
+
+    points_pad = torch.nn.functional.pad(points, (0, 0, 0, padding))
+    directions_pad = torch.nn.functional.pad(directions, (0, 0, 0, padding))
+
+    return field_outputs, masking, ids_shuffle, ids_restore, points_pad, directions_pad
+
+
+def spatial_sliced_batching(ray_samples: RaySamples, field_outputs: dict, batch_size: int, scene_bound: TensorType[2,3]):
     """
     Given the features, density, points and batch size, order the spatially in such a away that each batch is a box on an irregualar grid.
     Each cell contains batch size points.
@@ -112,7 +110,11 @@ def spatial_sliced_batching(ray_samples: RaySamples, field_outputs: dict, batch_
     masking = torch.cat((masking_top, masking_bottom)).to(device)
 
     points = ray_samples.frustums.get_positions()
-    rand_points = torch.randn((padding, 3), device=points.device)
+    scene_bound = scene_bound.to(points.device)
+    rand_points = torch.rand((padding, 3), device=points.device) * (scene_bound[1] - scene_bound[0]) + scene_bound[0]
+    # scale points to be closer to the scene bounds center by factor of 0.6
+    rand_points = (rand_points - scene_bound.mean(0)) * 0.6 + scene_bound.mean(0)
+    
     points_padded = torch.cat((points, rand_points))
 
     directions = ray_samples.frustums.directions